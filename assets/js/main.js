59cda039eadef8bbeae4d0e8dee39012b2f33ffe
julien tanguy <j.tanguy24@gmail.com>
2020-05-07 17:10:13 +0200

    modifs et debut js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,25 @@ $(document).ready(function () {
       $('.animated-icon3').toggleClass('open');
     });
   });
-
-  
+  //ANIMATED
+  const ratio = 0.9;
+      const options = {
+        //L'élément qui est utilisé comme zone d'affichage au moment d'évaluer la visibilité de la cible. si null, root = viewport
+        root: null,
+        rootMargin: '0px',
+        //indique à quel pourcentage de la visibilité de la cible la fonction handleIntersect de la cible doit être exécuté.
+        threshold: ratio
+      }
+      const handleIntersect = function (entries, observer) {
+        entries.forEach(function (entry) {
+          if (entry.intersectionRatio > ratio) {
+            console.log(entry.target);
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+          }
+        }); 
+      }
+      const observer = new IntersectionObserver(handleIntersect, options);
+      document.querySelectorAll('.reveal').forEach(function (r) {
+        observer.observe(r);
+      });
