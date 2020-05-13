52816a35ac90711c1063ad180d5ceb789a9621fa
julien tanguy <j.tanguy24@gmail.com>
2020-05-13 16:13:23 +0200

    modifs titres sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,22 +22,36 @@ const ratio = 0.9;
   document.querySelectorAll('.reveal').forEach(function (r) {
     observer.observe(r);
   });
-//----------------------------------------EFFET-CRITURE--------------------------------------------------
+//----------------------------------------EFFET-ECRITURE--------------------------------------------------
 const title = document.getElementById('autotext');
 const text = 'julien Tanguy, developpeur web';
 let index = 0;
-
 function play () {
     title.innerHTML = text.slice(0, index);
-
     index++;
-    //le if permet de rejour l'animation a l'infini.
+    //la condition permet de rejouer l'animation a l'infini.
     // if (index > text.length) {
     //     index = 0;
     // }
 }
-
 // setInterval permet d'appeller une fonction tout les x interval
-// ici tout les 300 ms, la fonction ply est relancé
+// ici tout les 300 ms, la fonction play est relancé
 let timer = setInterval(play, 300);
-    
+//----------------------------------------COMPETENCES-------------------------------------------------- 
+// window.onscroll = function(){
+//   var competenceList = document.getElementsByClassName('competence');
+//   var index = parseInt(window.pageYOffset / 50);
+//       if(index < paragraphList.length){
+//           competenceList[index].style.display = 'block';
+//       }
+// }
+// window.onscroll = function(){
+//   var html = document.getElementById('scrollHtml');
+//   var php = document.getElementById('scrollPhp');
+//   var js = document.getElementById('scrollJs');
+//   if (window.pageYOffset > 2000) {
+//     html.style.display = 'none';
+//   }else {
+//     html.style.display = 'block';
+//   }
+// }
